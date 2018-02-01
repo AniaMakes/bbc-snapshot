7a231fa9562dada1c7608bd3c04ccdba6a39e084
Ania <ania.makes@gmail.com>
2018-02-01 11:00:59 +0000

    Query selector exercises

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
-// write your exercise code here
+// Querying exercises part 1
+// console.log(document.getElementById("page-title").innerText);
+//
+// var listOfTopStories = document.getElementsByClassName("top-story__title");
+//
+// console.log(listOfTopStories);
+// Array.from(listOfTopStories).forEach(story => console.log(story.innerText));
+//
+//
+// var listOfImages = document.getElementsByTagName("img");
+//
+// console.log(listOfImages);
+//
+// Array.from(listOfImages).forEach(image => console.log(image.src));
+//
+// var orbAside = document.getElementById("orb-aside");
+// var arrayOfLinksInOrb = orbAside.getElementsByTagName("a");
+//
+// console.log(arrayOfLinksInOrb);
+//
+// Array.from(arrayOfLinksInOrb).forEach(aItem => console.log(aItem.href));
 
-console.log(document.getElementById("page-title").innerText);
+// Query selector exercises
 
-var listOfTopStories = document.getElementsByClassName("top-story__title");
+console.log(document.querySelector("#page-title").innerText);
 
-console.log(listOfTopStories);
+var listOfTopStories = document.querySelectorAll(".top-story__title");
+
 Array.from(listOfTopStories).forEach(story => console.log(story.innerText));
 
-
-var listOfImages = document.getElementsByTagName("img");
-
-console.log(listOfImages);
-
+var listOfImages = document.querySelectorAll("img");
 Array.from(listOfImages).forEach(image => console.log(image.src));
 
-var orbAside = document.getElementById("orb-aside");
-var arrayOfLinksInOrb = orbAside.getElementsByTagName("a");
-
-console.log(arrayOfLinksInOrb);
-
-Array.from(arrayOfLinksInOrb).forEach(aItem => console.log(aItem.href));
+var orbAsideAList = document.querySelector("#orb-aside").querySelectorAll("a");
+Array.from(orbAsideAList).forEach(orb => console.log(orb.href));
